0122e31ef9de7b19ffe3a32c84eef6db11564c09
chaninkou <michaelkoulove@yahoo.com>
2019-09-11 23:59:46 -0700

    Not done

---
+++ src/subsets/Main.java
@@ -0,0 +1,11 @@
+package subsets;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] nums = {3,2,1};
+		
+		System.out.println("Input: " + Arrays.toString(nums));
+	}
+}
