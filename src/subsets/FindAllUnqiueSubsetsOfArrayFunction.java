379fb902301ff2205ad5d3e9fc2e6540a68465cc
chaninkou <michaelkoulove@yahoo.com>
2019-11-17 23:31:20 -0800

    Great review on backtrack, no basecase, but update tempList and add list

--- src/subsets/FindAllUnqiueSubsetsOfArrayFunction.java
+++ src/subsets/FindAllUnqiueSubsetsOfArrayFunction.java
@@ -8,27 +8,28 @@ public class FindAllUnqiueSubsetsOfArrayFunction {
 	// Does not need to be sorted, unless it contains duplicate input
     public List<List<Integer>> subsets(int[] nums) {
     	// Contain total of all subset
-        List<List<Integer>> powerSet = new ArrayList<>();
+        List<List<Integer>> subsets = new ArrayList<>();
         
-        // Backtracking with 0 as starting point, pass in empty arraylist
-        backtrack(powerSet, new ArrayList<>(), nums, 0);
+        // Backtracking with 0 as starting point since we stast from index 0
+        backtrack(subsets, new ArrayList<>(), nums, 0);
         
-        return powerSet;
+        return subsets;
     }
     
-    private void backtrack(List<List<Integer>> powerSet, List<Integer> tempList, int[] nums, int start){
-    	// We are using new ArrayList cause we don't want it to reference to it
-    	powerSet.add(new ArrayList<>(tempList));
+    private void backtrack(List<List<Integer>> subsets, List<Integer> tempList, int[] nums, int start){
+    	// We create new ArrayList cause we don't want it to reference to it, we want a complete copy of it
+    	subsets.add(new ArrayList<>(tempList));
     	
     	// We try to get all the set from start and then move on to the next element
     	for(int i = start; i < nums.length; i++){
-    		// Update the tempList
+    		// Update the tempList, 
     		tempList.add(nums[i]);
     		
-    		// So we go down the list until we reach i = nums.length which will stop the loop
-    		backtrack(powerSet, tempList, nums, i + 1);
+    		// Reason why we using i + 1, so that we could backtrack the next element
+    		// Important to use i + 1, we don't do i++ cause we do want the next element too
+    		backtrack(subsets, tempList, nums, i + 1);
     		
-    		// Remove the last element of the tempList
+    		// Remove the last element of the tempList, since we got one of the subset
     		tempList.remove(tempList.size() - 1);
     	}
     	
